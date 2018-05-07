ce4de4de91e567771a3de9bc3c4ae0041aab9ad0
SilverMagpie <33327382+SilverMagpie@users.noreply.github.com>
2018-05-07 11:08:57 -0700

    Update led.py

--- led.py
+++ led.py
@@ -1,11 +1,10 @@
 from gpiozero import LED, Button
-from time import sleep
+from signal import pause
 
 led = LED(17)
 button = Button(19)
 
-button.wait_for_press()
-led.on()
-sleep(3)
-led.off()
+button.when_pressed = led.on
+button.when_released = led.off
 
+pause()
