a2f388c3684183ad2fc13f36aff98fe9a5f0cfb7
SilverMagpie <alexander.markwell@gmail.com>
2018-05-07 17:47:27 +0000

    Added the master list and a buzzer. We might want ot change the pin value.

--- music.py
+++ music.py
@@ -1,11 +1,16 @@
 from gpiozero import Buzzer
 from time import sleep
 
+beep = Buzzer(17)
+
 def wait(hertz):
-	return ((1 / hertz) / 2) * 1000000
+   return ((1 / hertz) / 2) * 1000
 
 songT = []
 waitT = []
 
 songS = []
 waitS = []
+
+song = songT + [s] + songS
+wait = waitT + [w] + waitS
