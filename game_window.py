f4da60ae4b9aceb625c85f97bf8696e964d97022
Alexander Markwell <alexander.markwell@gmail.com>
2018-05-30 11:14:21 -0700

    Created a window

--- game_window.py
+++ game_window.py
@@ -1,3 +1,9 @@
 import pygame
 
-print("Banana")
+screen = pygame.display.set_mode((500, 500))
+running = True
+
+while running:
+	event = pygame.event.poll()
+	if event.type == pygame.QUIT:
+		running = False
